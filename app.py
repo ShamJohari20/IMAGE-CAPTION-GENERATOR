c2f98b1895ae5e254664f0168f429797c9daa5a3
Sham Johari <122847705+ShamJohari20@users.noreply.github.com>
2023-07-05 14:46:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
-#from tqdm.notebook import tqdm
-#import matplotlib.pyplot as plt
+from tqdm.notebook import tqdm
+import matplotlib.pyplot as plt
 
-#from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
-#from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.models import Model
 from tensorflow.keras.utils import to_categorical, plot_model
 from tensorflow.keras.layers import Input, Dense, LSTM, Embedding, Dropout, add
